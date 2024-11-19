834c40cf9a61e6c89f54516fdaacc23a597428ac
Andi <theodorusandi.g@gmail.com>
2024-11-18 22:13:16 -0500

    add debounce

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,33 +1,57 @@
-function removeShorts() {
-  const ytdRichSectionRenderers = document.querySelectorAll("ytd-rich-section-renderer");
+
+export function filterContent(hide = true) {
+  function filterShorts() {
+    const ytdRichSectionRenderers = document.querySelectorAll("ytd-rich-section-renderer");
 
     ytdRichSectionRenderers.forEach((ytdRichSectionRenderer) => {
-      const shorts = ytdRichSectionRenderer.querySelector("ytd-rich-shelf-renderer[is-shorts]");
-
-      if (shorts) {
+      if (ytdRichSectionRenderer.querySelector("ytd-rich-shelf-renderer[is-shorts]")) {
         ytdRichSectionRenderer.style.display = "none";
       }
     })
+  }
+
+  function filterNonHighlights(hide) {
+    const NHL_REGEX = /\bNHL Highlights\b/i;
+    const MLB_REGEX = /\bMLB Highlights\b/i;
+
+    const ytdRichItemRenderers = document.querySelectorAll("ytd-rich-item-renderer");
+
+    ytdRichItemRenderers.forEach((ytdRichItemRenderer) => {
+      const title = ytdRichItemRenderer.querySelector("#video-title")?.textContent || "";
+      const nhlChannel = ytdRichItemRenderer.querySelector("a[href='/@NHL']");
+      const mlbChannel = ytdRichItemRenderer.querySelector("a[href='/@MLB']");
+
+      const highlights = (nhlChannel && NHL_REGEX.test(title)) || (mlbChannel && MLB_REGEX.test(title));
+
+      if (!highlights) {
+        ytdRichItemRenderer.style.display = hide ? "none" : "block";
+      }
+    })
+  }
+
+
+  filterShorts();
+  filterNonHighlights(hide);
+
+  if (hide) {
+    const mutationObserver = new MutationObserver((mutationsList) => {
+      let timeout;
+      mutationsList.forEach((mutation) => {
+        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+          clearTimeout(timeout);
+          timeout = setTimeout(() => {
+            filterShorts();
+            filterNonHighlights(hide);
+          }, 500)
+        }
+      });
+    });
+
+    const observerTarget = document.querySelector("div#contents") || document.body;
+
+    mutationObserver.observe(observerTarget, {
+      childList: true,
+      subtree: true
+    });
+  }
 }
-
-function filterContent() {
-  const NHL_REGEX = /\bNHL Highlights\b/i;
-  const MLB_REGEX = /\bMLB Highlights\b/i;
-  
-  const ytdRichItemRenderers = document.querySelectorAll("ytd-rich-item-renderer");
-
-  ytdRichItemRenderers.forEach((ytdRichItemRenderer) => {
-    const title = ytdRichItemRenderer.querySelector("#video-title")?.textContent || "";
-    const nhlChannel = ytdRichItemRenderer.querySelector("a[href='/@NHL']");
-    const mlbChannel = ytdRichItemRenderer.querySelector("a[href='/@MLB']");
-
-    const highlights = (nhlChannel && NHL_REGEX.test(title)) || (mlbChannel && MLB_REGEX.test(title));
-
-    if (!highlights) {
-      ytdRichItemRenderer.style.display = "none";
-    }
-  })
-}
-
-removeShorts();
-filterContent();
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
-const filterSwitch = document.querySelector("#filterButton");
+import { filterContent } from "./filter.js";
 
-filterSwitch.addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-    const curTab = tabs[0];
-    if (curTab) {
-      await chrome.scripting.executeScript({
-        target: { tabId: curTab.id },
-        files: ["scripts/filter.js"]
-      })
-    }
-  })
+const filterButton = document.querySelector("#filterButton");
+
+filterButton.addEventListener("change", (e) => {
+  const { checked } = e.target;
+
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, async (tabs) => {
+    const tabId = tabs[0]?.id || "";
+
+    await chrome.scripting.executeScript({
+      target: { tabId },
+      func: filterContent,
+      args: [checked]
+    })
+  });
 });
